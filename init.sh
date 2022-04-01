270819934bec4b73d5d1fcbb2d9908b668a5e3b5
P-Mihail <mihail.parushkin@yandex.ru>
2022-04-02 01:08:41 +0700

    mod init.sh for ex 2

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/gunicorn.conf
+exec gunicorn -с ../etc/gunicorn.conf ask.wsgi:application
