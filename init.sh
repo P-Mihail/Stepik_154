cf6034002108ec8b0ab686fa8e074f94c0c15fe1
P-Mihail <mihail.parushkin@yandex.ru>
2022-04-18 15:36:36 +0700

    bash script for ex 2

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf.py  /etc/gunicorn.d/gunicorn.conf.py
+exec gunicorn -c /etc/gunicorn.d/gunicorn.conf.py hello:app
