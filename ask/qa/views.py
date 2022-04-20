4d466758ab0d1beab87c24d9a3053a492e77b14d
P-Mihail <mihail.parushkin@yandex.ru>
2022-04-21 01:09:27 +0700

    тестовый контроллер с параметром

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def test(request, *args, **kwargs):
     return HttpResponse("OK")
+
+
+def test_question_id(request, question_id):
+    return HttpResponse(f"OK {question_id}")
