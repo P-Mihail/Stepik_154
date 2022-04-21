6b6a0326eda0ce3f555aecd7fe288e7edca831c3
P-Mihail <mihail.parushkin@yandex.ru>
2022-04-21 17:54:34 +0700

    bash script for EX 3

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
+sudo pip3 install Django==2.0.1
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf.py  /etc/gunicorn.d/gunicorn-wsgi.conf.py
-exec gunicorn -D -c /etc/gunicorn.d/gunicorn-wsgi.conf.py hello:app
+exec gunicorn -D -c /home/box/web/etc/gunicorn-wsgi.conf.py hello:app &
+cd /home/box/web/ask/ 
+exec gunicorn -D -c ../etc/gunicorn-django.conf.py ask.wsgi:application &
